3de3dc47bc502691c16bd179d778827cce84a490
Edward Thomson <ethomson@microsoft.com>
2013-03-21 14:02:25 -0700

    don't convert CRLF to CRCRLF

--- tests-clar/checkout/crlf.c
+++ tests-clar/checkout/crlf.c
@@ -10,7 +10,9 @@
 #define MORE_CRLF_TEXT_RAW	"crlf\r\ncrlf\r\nlf\ncrlf\r\ncrlf\r\n"
 #define MORE_LF_TEXT_RAW	"lf\nlf\ncrlf\r\nlf\nlf\n"
 
-#define ALL_LF_TEXT_AS_CRLF	"lf\r\nlf\r\nlf\r\nlf\r\nlf\r\n"
+#define ALL_LF_TEXT_AS_CRLF		"lf\r\nlf\r\nlf\r\nlf\r\nlf\r\n"
+#define MORE_CRLF_TEXT_AS_CRLF	"crlf\r\ncrlf\r\nlf\r\ncrlf\r\ncrlf\r\n"
+#define MORE_LF_TEXT_AS_CRLF	"lf\r\nlf\r\ncrlf\r\nlf\r\nlf\r\n"
 
 static git_repository *g_repo;
 
@@ -70,6 +72,48 @@ void test_checkout_crlf__detect_crlf_autocrlf_true(void)
 	git_checkout_head(g_repo, &opts);
 
 	test_file_contents("./crlf/all-lf", ALL_LF_TEXT_AS_CRLF);
+#endif
+}
+
+void test_checkout_crlf__more_lf_autocrlf_true(void)
+{
+#ifdef GIT_WIN32
+	git_checkout_opts opts = GIT_CHECKOUT_OPTS_INIT;
+	opts.checkout_strategy = GIT_CHECKOUT_SAFE_CREATE;
+
+	set_core_autocrlf_to(true);
+
+	git_checkout_head(g_repo, &opts);
+
+	test_file_contents("./crlf/more-lf", MORE_LF_TEXT_AS_CRLF);
+#endif
+}
+
+void test_checkout_crlf__more_crlf_autocrlf_true(void)
+{
+#ifdef GIT_WIN32
+	git_checkout_opts opts = GIT_CHECKOUT_OPTS_INIT;
+	opts.checkout_strategy = GIT_CHECKOUT_SAFE_CREATE;
+
+	set_core_autocrlf_to(true);
+
+	git_checkout_head(g_repo, &opts);
+
+	test_file_contents("./crlf/more-crlf", MORE_CRLF_TEXT_AS_CRLF);
+#endif
+}
+
+void test_checkout_crlf__all_crlf_autocrlf_true(void)
+{
+#ifdef GIT_WIN32
+	git_checkout_opts opts = GIT_CHECKOUT_OPTS_INIT;
+	opts.checkout_strategy = GIT_CHECKOUT_SAFE_CREATE;
+
+	set_core_autocrlf_to(true);
+
+	git_checkout_head(g_repo, &opts);
+
+	test_file_contents("./crlf/all-crlf", ALL_CRLF_TEXT_RAW);
 #endif
 }
 
